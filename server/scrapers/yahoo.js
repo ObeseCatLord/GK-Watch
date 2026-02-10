b093082560ac04635533a500bd19e9ceb1430ec6
GK Watcher <gkwatcher@example.com>
2026-02-10 15:12:56 -0600

    fix: increase yahoo native scraper timeout to 30s

--- server/scrapers/yahoo.js
+++ server/scrapers/yahoo.js
@@ -356,7 +356,7 @@ async function search(query, strictEnabled = true, allowInternationalShipping = 
                     headers: {
                         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
                     },
-                    timeout: 10000, // 10s timeout to prevent hangs
+                    timeout: 30000, // 30s timeout to prevent hangs
                     validateStatus: function (status) {
                         return status < 500;
                     }
