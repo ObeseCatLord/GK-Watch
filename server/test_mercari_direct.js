fb1b4b0b0796d3886696f319a37ec4793679da91
GK Watcher <gkwatcher@example.com>
2026-07-08 13:53:20 -0500

    Remove local debug artifacts from tracking

--- server/test_mercari_direct.js
+++
@@ -1,8 +0,0 @@
-const mercari = require('./scrapers/mercari');
-
-(async () => {
-    console.log("Testing Mercari Scraper Direct Import...");
-    const results = await mercari.search("test");
-    console.log(`Results found: ${results.length}`);
-    process.exit(0);
-})();
